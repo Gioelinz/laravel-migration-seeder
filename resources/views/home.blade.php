90fc9dc3f93e6eb5a98b4c13d8e0b2c3a932a4b4
Gioelinz <gioelerunza.gr@gmail.com>
2022-03-18 17:13:48 +0100

    finished layout e prin from db

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,16 +7,25 @@
             <div class="card-body">
                 <h5 class="card-title">Company: {{ $train->company }}</h5>
                 <h6 class="card-subtitle mb-2 text-muted">Train Code: {{ $train->train_code }}</h6>
-                <p class="card-text"><strong>Departure:</strong>
+                <p class="card-text mb-0"><strong>Departure:</strong>
                     {{ date('F j, Y, g:i a ', strtotime($train->departure_hour)) }}
                 </p>
-                <p class="card-text"><strong>Arrival:</strong>
+                <p class="card-text"><strong>From:</strong>
+                    {{ $train->departure_station }}
+                </p>
+                <p class="card-text mb-0"><strong>Arrival:</strong>
                     {{ date('F j, Y, g:i a ', strtotime($train->arrive_hour)) }}
                 </p>
+                <p class="card-text"><strong>To:</strong>
+                    {{ $train->arrive_station }}
+                </p>
                 <h6 class="card-subtitle mb-2">Carriages: {{ $train->carriages }}</h6>
-                <h6 class="card-subtitle mb-2 {{ $train->in_time ? 'text-success' : 'text-danger' }}">
-                    {{ $train->in_time ? 'In orario' : 'In ritardo' }}</h6>
-
+                @if (!$train->cancelled)
+                    <h5 class="card-subtitle mb-2 {{ $train->in_time ? 'text-success' : 'text-danger' }}">
+                        {{ $train->in_time ? 'In time' : 'late' }}</h5>
+                @else
+                    <p class="btn btn-danger">Deleted</p>
+                @endif
             </div>
         </div>
 
