c942bae45f082dec1a9131f77f61beaae375572a
Gioelinz <gioelerunza.gr@gmail.com>
2022-03-18 14:53:47 +0100

    connection to db

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,11 +5,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Trains</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>{{ env('APP_NAME') }}</title>
 </head>
 
 <body>
-
+    <header>
+        <nav class="navbar navbar-light bg-info">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Transilien-train.svg/1200px-Transilien-train.svg.png"
+                        alt="" width="60" height="60">
+                    Trains
+                </a>
+            </div>
+        </nav>
+    </header>
 </body>
 
 </html>
