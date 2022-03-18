dceea3aa7c9d1b16bc2f68ff95b7ad53fa30a5f5
Gioelinz <gioelerunza.gr@gmail.com>
2022-03-18 17:40:31 +0100

    add now trains with fill seeder

--- app/Models/Train.php
+++ app/Models/Train.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Train extends Model
 {
-    //
+    protected $fillable = [
+        'company',
+        'departure_station',
+        'arrive_station',
+        'departure_hour',
+        'arrive_hour',
+        'train_code',
+        'carriages',
+        'in_time',
+        'cancelled',
+    ];
 }
---
+++ config/trains.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+    [
+        'company' => 'Italo',
+        'departure_station' => 'Catania',
+        'arrive_station' => 'Milano',
+        'departure_hour' => '2022-03-18 18:00:00',
+        'arrive_hour' => '2022-03-19 06:30:00',
+        'train_code' => '982651',
+        'carriages' => '5',
+        'in_time' => 1,
+        'cancelled' => 0,
+    ],
+    [
+        'company' => 'Trenord',
+        'departure_station' => 'Napoli',
+        'arrive_station' => 'Catania',
+        'departure_hour' => '2022-03-18 10:00:00',
+        'arrive_hour' => '2022-03-18 18:15:00',
+        'train_code' => '882634',
+        'carriages' => '4',
+        'in_time' => 0,
+        'cancelled' => 0,
+    ],
+    [
+        'company' => 'Trenitalia',
+        'departure_station' => 'Milano Centrale',
+        'arrive_station' => 'Genova',
+        'departure_hour' => '2022-03-20 10:00:00',
+        'arrive_hour' => '2022-03-20 12:45:00',
+        'train_code' => '712634',
+        'carriages' => '4',
+        'in_time' => 0,
+        'cancelled' => 1,
+    ],
+    [
+        'company' => 'Italo',
+        'departure_station' => 'Verona Porta Nuova',
+        'arrive_station' => 'Firenze Santa Maria Novella',
+        'departure_hour' => '2022-03-18 10:00:00',
+        'arrive_hour' => '2022-03-18 15:16:00',
+        'train_code' => '635209',
+        'carriages' => '6',
+        'in_time' => 0,
+        'cancelled' => 0,
+    ]
+];
